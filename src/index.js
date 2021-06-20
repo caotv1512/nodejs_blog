9faa27cd29cf939f3e20ed08800680dc04d42984
Tran Van Cao <caomanhct@gmail.com>
2021-06-20 15:35:44 +0700

    MVC refactor code: create routes, controller folder

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
-
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template Engine
 app.engine('hbs', handlebars({
@@ -18,15 +21,9 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
-console.log(path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
